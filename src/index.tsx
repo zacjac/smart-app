48ddce311a8c77a5b5dad70966358b11bf351e72
zacjac <m.e.zacjac@gmail.com>
2022-07-23 06:48:01 -0700

    project setup

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "./config/tailwind.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
